aba5f41011219bcaaed115a6cfb9e6111f526bca
Mike Ly <mike.ly@deliveroo.co.uk>
2018-07-27 13:26:39 +0100

    refactored getClient

--- app.go
+++ app.go
@@ -8,22 +8,27 @@ import (
   "os"
 )
 
-func main() {
-  ctx := context.Background()
+func getClient(ctx context.Context, token string) *github.Client {
   ts := oauth2.StaticTokenSource(
-    &oauth2.Token{AccessToken: "TOKEN"},
+    &oauth2.Token{AccessToken: token},
   )
   tc := oauth2.NewClient(ctx, ts)
 
-  // get go-github client
-  client := github.NewClient(tc)
+  return github.NewClient(tc)
+}
 
+func main() {
+
+  // get go-github client
+  ctx := context.Background()
+  client := getClient(ctx, "TOKEN")
+
+  result, _, err := client.Activity.ListNotifications(ctx, nil)
   // opts :=  &github.SearchOptions{Sort: "created", Order: "asc"}
   // result, _, err := client.Search.Issues(ctx, "is:open+is:pr+review-requested:hammerfunk", opts)
   // result, _, err := client.Search.Issues(ctx, "windows+label:bug+state:open", opts)
   // result, _, err := client.Search.Users(ctx, "tom+repos:%3E42+followers:%3E1000", nil)
   // result, _, err := client.Search.Users(ctx, "hammerfunk", nil)
-  result, _, err := client.Activity.ListNotifications(ctx, nil)
   // https://api.github.com/search/issues/?q\=is:open+is:pr+review-requested:annebyrne
   // result, _, err := client.Organizations.List(ctx, "hammerfunk", nil)
 
