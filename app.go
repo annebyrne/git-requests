7a552eaae7f7efe4349d723a7f3139e7bf705ea2
Mike Ly <mike.ly@deliveroo.co.uk>
2018-07-27 13:09:52 +0100

    Fetch some github stuff

--- app.go
+++ app.go
@@ -18,12 +18,19 @@ func main() {
   // get go-github client
   client := github.NewClient(tc)
 
-  orgs, _, err := client.Repositories.List(ctx, "hammerfunk", nil)
+  // opts :=  &github.SearchOptions{Sort: "created", Order: "asc"}
+  // result, _, err := client.Search.Issues(ctx, "is:open+is:pr+review-requested:hammerfunk", opts)
+  // result, _, err := client.Search.Issues(ctx, "windows+label:bug+state:open", opts)
+  // result, _, err := client.Search.Users(ctx, "tom+repos:%3E42+followers:%3E1000", nil)
+  // result, _, err := client.Search.Users(ctx, "hammerfunk", nil)
+  result, _, err := client.Activity.ListNotifications(ctx, nil)
+  // https://api.github.com/search/issues/?q\=is:open+is:pr+review-requested:annebyrne
+  // result, _, err := client.Organizations.List(ctx, "hammerfunk", nil)
 
   if err != nil {
     fmt.Printf("Problem in getting repository information %v\n", err)
     os.Exit(1)
   }
 
-  fmt.Printf("RESULT orgs %v\n", orgs)
+  fmt.Printf("RESULT result %v\n", result)
 }
